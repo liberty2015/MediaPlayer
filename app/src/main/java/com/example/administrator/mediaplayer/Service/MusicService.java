f10090ac10ef667bc91b7c60c7c8008e80985307
Liberty <409947972@qq.com>
2016-11-20 13:22:08 +0800

    new

---
+++ app/src/main/java/com/example/administrator/mediaplayer/Service/MusicService.java
@@ -0,0 +1,105 @@
+package com.example.administrator.mediaplayer.Service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2016/11/19.
+ */
+
+public class MusicService extends Service
+    implements MediaPlayer.OnPreparedListener,
+    MediaPlayer.OnErrorListener,
+    MediaPlayer.OnCompletionListener{
+
+    private MediaPlayer mediaPlayer;
+    private int currentProgress=0;
+
+    @Override
+    public void onCompletion(MediaPlayer mediaPlayer) {
+
+    }
+
+    class MusicBinder extends Binder {
+
+        public void play(String url){
+            try {
+                mediaPlayer.setDataSource(url);
+                mediaPlayer.prepareAsync();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public void stop(){
+            if (mediaPlayer.isPlaying()){
+                mediaPlayer.stop();
+            }
+        }
+
+        public void pause(){
+            if (mediaPlayer.isPlaying()){
+                currentProgress=mediaPlayer.getCurrentPosition();
+                mediaPlayer.pause();
+            }else {
+                mediaPlayer.seekTo(currentProgress);
+                mediaPlayer.start();
+            }
+        }
+
+        public int getCurrentPosition(){
+            return mediaPlayer.getCurrentPosition();
+        }
+    }
+
+    private void initMediaPlayer(){
+        mediaPlayer=new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnCompletionListener(this);
+        mediaPlayer.setOnErrorListener(this);
+        mediaPlayer.setOnPreparedListener(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initMediaPlayer();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return new MusicBinder();
+    }
+
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mediaPlayer.release();
+        mediaPlayer=null;
+    }
+
+    @Override
+    public boolean onError(MediaPlayer mediaPlayer, int i, int i1) {
+        return false;
+    }
+}
---
+++ app/src/main/java/com/example/administrator/mediaplayer/widget/CDView.java
@@ -0,0 +1,16 @@
+package com.example.administrator.mediaplayer.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Administrator on 2016/11/20.
+ */
+
+public class CDView extends View {
+    public CDView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
